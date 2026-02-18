bdd7dbe48270b757ab3ea7c6b1d0a533d0a39e45
NathouneOne <nathan.marin.fx@gmail.com>
2026-02-18 20:33:52 +0100

    Project creation

---
+++ game-jam-0226/Scenes/Main/main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+## Project launch with main, which handles Menus / UXs / and loading - lauch - reset game etc, potential saves etc etc ?
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
