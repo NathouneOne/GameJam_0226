9bad89b7f3032e51563d2364253ae6e2325a7707
nohehf <nohe.hinniger.foray@gmail.com>
2026-02-19 15:08:45 +0100

    feat: control arm with mouse

--- game-jam-0226/Scenes/hand/hand.gd
+++ game-jam-0226/Scenes/hand/hand.gd
@@ -1,13 +1,7 @@
 extends Node2D
 
 var closed: bool
-
-enum HAND_TYPE {
-	LEFT,
-	RIGHT
-}
-@export var hand_type: HAND_TYPE = HAND_TYPE.LEFT
-@export var move_speed: float = 400.0
+@export var follow_speed: float = 15.0
 
 var grabbed_object: Node2D = null
 var grabbable_component: Grabbable = null
@@ -15,23 +9,17 @@ var grabbable_component: Grabbable = null
 @onready var area_2d: Area2D = $Area2D
 
 
-## Returns the keymap prefix for the current hand (e.g. "leftHand_" or "rightHand_").
-func keymap_key(key: String) -> String:
-	return "leftHand_" + key if hand_type == HAND_TYPE.LEFT else "rightHand_" + key
-
 func _ready() -> void:
 	closed = false;
 
 
 func _process(delta: float) -> void:
-	var direction := Vector2(
-		Input.get_axis(keymap_key("left"), keymap_key("right")),
-		Input.get_axis(keymap_key("top"), keymap_key("bot"))
-	)
-	position += direction * move_speed * delta
+	var target_pos: Vector2 = get_global_mouse_position()
+	global_position = global_position.lerp(target_pos, follow_speed * delta)
+	
 	_process_grabbed_object()
 	
-	if Input.is_action_just_pressed(keymap_key("action")):
+	if Input.is_action_just_pressed("left_clic"):
 		if grabbed_object:
 			_release_object()
 		else:
