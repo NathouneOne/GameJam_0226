dc305304ef37698e6f18d361b9e533e1e0efdf2c
nohehf <nohe.hinniger.foray@gmail.com>
2026-02-19 17:33:21 +0100

    --wip-- [skip ci]

---
+++ game-jam-0226/Scenes/Components/usable/usable.gd
@@ -0,0 +1,41 @@
+extends Area2D
+class_name Usable
+
+signal grabbed(hand: Node2D)
+signal released()
+
+var is_grabbed: bool = false
+var hand_grabbing: Node2D = null
+
+func _ready() -> void:
+	add_to_group(&"grabbables")
+
+# return true if grabbed
+func grab(hand: Node2D) -> bool:
+	if is_grabbed:
+		return true
+	is_grabbed = true
+	hand_grabbing = hand
+	grabbed.emit(hand)
+	return true
+
+
+# Returns true if released
+func release() -> bool:
+	if not is_grabbed:
+		return true
+	
+	var over_zone: bool = false
+	var areas: Array[Area2D] = get_overlapping_areas()
+	for area: Area2D in areas:
+		if area.is_in_group(&"item_zone"):
+			over_zone = true
+			break
+	
+	if not over_zone:
+		return false
+		
+	is_grabbed = false
+	hand_grabbing = null
+	released.emit()
+	return true
