bfd7db9d97e973ce9ca83e94f7a46c1c38df093c
nohehf <nohe.hinniger.foray@gmail.com>
2026-02-20 21:20:39 +0100

    feat: menu animation banana

--- game-jam-0226/Scenes/Components/autoloads/globals.gd
+++ game-jam-0226/Scenes/Components/autoloads/globals.gd
@@ -6,9 +6,11 @@ signal on_new_patient()
 signal on_end_game()
 signal on_add_score(score: int)
 
+signal hand_force_release_object()
 
 var high_score := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	on_start_game.emit()
+	pass
+	#on_start_game.emit()
