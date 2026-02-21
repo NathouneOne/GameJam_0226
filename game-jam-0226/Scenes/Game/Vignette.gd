815b30fb8674c8c148b559a214d35987ba9b2139
NathouneOne <nathan.marin.fx@gmail.com>
2026-02-21 16:16:06 +0100

    vognette

---
+++ game-jam-0226/Scenes/Game/Vignette.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@onready var ellapsedtime = 60-%GameTime.remaining_time_seconds
+var opacityValue :float =0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	ellapsedtime = 60-%GameTime.remaining_time_seconds
+	opacityValue = ellapsedtime/60
+	%VignetteRouge.modulate.a=opacityValue
+	print(%GameTime.remaining_time_seconds)
+	
+	
